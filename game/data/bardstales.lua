d21d1dc5a42c2cebd7217e1039425658ef6aca45
Alexander <alexyecu@localhost.localdomain>
2015-08-15 11:06:40 +0300

    bugfixes

---
+++ game/data/bardstales.lua
@@ -0,0 +1,15 @@
+function bardstales_load ()
+
+instruments={lute=504,guitar=505,harmonica=506,drum=507,tomtom=508,tambourine=509,violin=510,clavecin=511,flute=512,handorgan=513,harp=514,bagpip=515,lipharmonica=516,maracas=517,xylophone=518};
+
+samples_ttx={
+{id=1,
+instrument="lute",
+sound=nil,
+title="",
+words="",
+emotions={},
+},
+
+}
+end;
